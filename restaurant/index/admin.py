561ea3a0a0f345e7f9091f859038100da65150ed
Namrata7199 <namrataladda@gmail.com>
2018-09-09 16:29:51 +0530

    created database

--- restaurant/index/admin.py
+++ restaurant/index/admin.py
@@ -3,9 +3,12 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-from .models import rest_menu,restaurant,owner
+from .models import rest_menu,restaurant,owner,user,reviews,search_by_cuisine
 
 # Register your models here.
 admin.site.register(rest_menu)
 admin.site.register(restaurant)
 admin.site.register(owner)
+admin.site.register(user)
+admin.site.register(reviews)
+admin.site.register(search_by_cuisine)
--- restaurant/index/models.py
+++ restaurant/index/models.py
@@ -10,17 +10,47 @@ class rest_menu(models.Model):
 	cuisine = models.CharField(max_length=20)
 
 class restaurant(models.Model):
+	gstin = models.CharField(primary_key=True,max_length=20)
 	name = models.CharField(max_length=30)
 	dno = models.CharField(max_length=10)
 	street = models.CharField(max_length=30)
 	city = models.CharField(max_length=30)
 	phn = models.IntegerField()
 	images = models.ImageField(max_length=10000)
-	gstin = models.CharField(primary_key=True,max_length=20)
 	menupk = models.ForeignKey(rest_menu,on_delete=models.CASCADE)
+	cuisine = models.CharField(max_length=30)
+
+	class meta:
+		db_table = 'restaurant'
+		unique_together = (('name','dno','street','city','phn','images'),)
 
 class owner(models.Model):
 	first_name = models.CharField(max_length=30)
 	middle_name = models.CharField(max_length=30)
 	last_name = models.CharField(max_length=30)
 	gstin = models.ForeignKey(restaurant,on_delete=models.CASCADE)
+
+class reviews(models.Model):
+	ratings = models.IntegerField()
+	Feedback = models.CharField(max_length=1000)
+	gstin = models.ForeignKey(restaurant,on_delete=models.CASCADE)
+	userpk = models.ForeignKey(owner,on_delete=models.CASCADE)
+
+class user(models.Model):
+	email = models.CharField(primary_key=True,max_length = 30)
+	first_name = models.CharField(max_length=30)
+	middle_name = models.CharField(max_length=30)
+	last_name = models.CharField(max_length=30)
+	dno = models.CharField(max_length=10)
+	street = models.CharField(max_length=30)
+	city = models.CharField(max_length=30)
+	phn = models.IntegerField()
+ 	rest = models.ForeignKey(restaurant,on_delete=models.CASCADE)
+
+class search_by_cuisine(models.Model):
+	gstin = models.ForeignKey(restaurant,on_delete=models.CASCADE)
+	email = models.ForeignKey(user,on_delete=models.CASCADE)
+
+
+
+    
