dfbf0bd6707a010b6e0e6e5a5b231cd0b728e2d4
Namrata7199 <namrataladda@gmail.com>
2020-05-27 12:01:05 +0530

    small changes

--- restaurant/restaurant/urls.py
+++ restaurant/restaurant/urls.py
@@ -27,8 +27,8 @@ urlpatterns = [
 	url(r'^admin/', admin.site.urls),
 	url(r'^signup/$',profile_views.signup, name='signupasuser'),
     url(r'^restaurants/',include('rest_page.urls')),
-	url(r'^login/$',auth_views.login,{'template_name':'login.html'}, name='login'),
-    url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
+	url(r'^login/$',auth_views.LoginView,{'template_name':'login.html'}, name='login'),
+    url(r'^logout/$', auth_views.LogoutView, {'next_page': '/'}, name='logout'),
     url(r'^user/$',profile_views.view_profile,name="view_profile"),
     url(r'^form/$',profile_views.fill_form,name="fill_form"),
     ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
